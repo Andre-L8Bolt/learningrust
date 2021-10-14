b636cce0534036c284f7a5fa25859e5d619fca5b
André-L8Bolt <andre.wandeler@me.com>
2021-10-13 22:30:58 -0400

    Add helpful details & Add elice type example

--- rusttutorial/src/main.rs
+++ rusttutorial/src/main.rs
@@ -24,12 +24,16 @@ fn main() {
 
 fn simpletesting() {
     println!("Hello, world!");
+    println!("{} {} {}\n\n", "this includes simple concepts and syntax".yellow(), "enjoy it, but", "for more info read THE BOOK".red().bold());
+    //above a library was used for color, it is just a message
     let i32num: i16 = -255; //creates new var as signed 16-bit int
     let unsignedarch: usize = 4_000_000_000; //creates new unsigned architecture-max int //can use _'s as separator
     let float8: f32 = 3.5; //creates new single-precision float
     let tru: bool = true; //creates new boolean with value true
     let aint = 5; //creates new int (compiler can often determine type at compile-time
     let strlit = "I am a rustation"; //creates a new string literal (on stack instead of heap)
+    let mut string = String::from("I had duck for"); //creates new string from literal
+
     let mut mutable = 8; //variables are not mutable by default (adding mut changes that)
     mutable = mutable + 1;
     println!("i32num is {} and unsignedarch is {}", i32num, unsignedarch);
@@ -37,8 +41,19 @@ fn simpletesting() {
     println!("a bool is {}", tru);
     println!("aint is {}", aint); //print's and println's act in a similar way to how they do in java
     println!("stringlit is: {}", strlit);
+    println!("the value of newly created and assigned string is: {}", string);
+    string.push(' '); //adds space char (note single quotes)
+    string.push_str("dinner…"); //adds string literal to string end
+    println!("the value of mutable string is now: {}", string.italic());
     println!("a mutable var was 8, but now is: {}", mutable);
-    //add slice type (delete once done)
+    println!("there are slices similar to java (indexing), they are part of a string");
+    let len = string.chars().count() -1; //let equals the length of string (and subtracts) (using .len does not always return correct value (unicode)
+    let slice = &string[6..len]; //creates slice variable(&str), from 8. Takes reference as input (as actual value is not know at compilation)
+    println!("The slice of 6 to the length -1 is: {}", slice);
+    let slice = &string[..]; //shadows slice (compiler can determine type) (see 2 lines above)
+    println!("You can also take a slice of the entire string: {}", slice);
+
+    println!("\n{} {} {}\n", "make sure to check".yellow(), "rustlang docs (methods)".green().bold(), "and THE BOOK".red());
 }
 
 fn ownershipandstructstesting() {
@@ -56,7 +71,7 @@ fn guessgame() { //new function called guessgame (from the tutorial)
         io::stdin().read_line(&mut guess).expect("Failed to read input"); //same as in selection loop
         let guess: i8 = match guess.trim().parse() { //shadows the var guess
             Ok(num) => {
-                match num {
+                match num { //needed due to i8 selection (u32 is standard), saves some mem, but should do convention
                     1..=25 => num,
                     _ => continue,
                 }
